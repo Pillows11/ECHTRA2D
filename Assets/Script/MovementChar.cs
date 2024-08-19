0c6d6ab2ad9b2febfdbc9f8ed4b83c97cda9602e
Pillows11 <pillowskun11@gmail.com>
2024-08-19 11:15:21 +0700

    new

--- Assets/Script/MovementChar.cs
+++ Assets/Script/MovementChar.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementChar : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
+    public Transform groundCheck;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        // Pengecekan apakah Rigidbody2D ada di GameObject
+        if (rb == null)
+        {
+            Debug.LogError("No Rigidbody2D found on " + gameObject.name);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (rb != null)
+        {
+            float moveInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
     }
 }
